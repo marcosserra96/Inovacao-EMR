75f09d01fc5e444f98598cf7e07db2f40eaffa2b
marcosserra96 <marcospauloserra@outlook.com.br>
2025-09-30 20:58:07 -0300

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,10 +1,10 @@
-// firebase.js — compatível com navegador e GitHub Pages
+// firebase.js — compatível com navegador (GitHub Pages)
 
 // Configuração do seu projeto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyC2l8LU3vYfQjTly8JSa658mfIlVk2Dw8E",
   authDomain: "inovacao-emr.firebaseapp.com",
-  databaseURL: "https://inovacao-emr-default-rtdb.firebaseio.com", // essencial!
+  databaseURL: "https://inovacao-emr-default-rtdb.firebaseio.com", // Realtime Database URL
   projectId: "inovacao-emr",
   storageBucket: "inovacao-emr.appspot.com",
   messagingSenderId: "1075399271811",
@@ -15,6 +15,5 @@ const firebaseConfig = {
 // Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Exporta referência para o banco de dados
-const db = firebase.database();
-
+// Disponibiliza o db globalmente
+window.db = firebase.database();
